4a5e8a44270da957ecc1eef25cf9684b807d3b43
bossmaw <wausan_230000001410@uic.edu.ph>
2025-10-30 05:03:21 +0800

    Update chat.dart

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,1 +1,12 @@
-c
+class Message {
+  final String sender;
+  final String text;
+  final DateTime time;
+  final bool isMe;
+
+  Message(
+      {required this.sender,
+        required this.text,
+        required this.time,
+        this.isMe = false});
+}
